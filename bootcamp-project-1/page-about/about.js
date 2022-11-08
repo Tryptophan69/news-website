47a0beb7ae9ad861ad57c8dce4f62d44b52b1bea
Tryptophan69 <100468551+Tryptophan69@users.noreply.github.com>
2022-11-08 16:06:06 +0530

    Add files via upload

---
+++ bootcamp-project-1/page-about/about.js
@@ -0,0 +1,22 @@
+
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("sticky-nav");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset -60 >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("collapse-navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset -90 >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
